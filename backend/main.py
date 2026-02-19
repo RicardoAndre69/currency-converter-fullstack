5215acdc4846dd7506e66df60279ca121af3ece6
CIEEMG - Ricardo Andre da Silva Filho <ricardo.andre@cieemg.bhz>
2026-02-19 17:15:54 -0300

    fixed railpack

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -5,6 +7,10 @@ import requests
 
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 app.add_middleware(
     CORSMiddleware,
