87af96a417c3ff253cb9cee63512e3bc86d83059
Ricardo Andre <ricasilvafilho@gmail.com>
2026-02-20 23:51:13 -0300

    dockerfile backend cmd changes port

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Usar o valor de PORT, ou usar 8000 como padrão se não estiver definido
-PORT=${PORT:-8000}
+PORT=${PORT:-8080}
 
 # Iniciar o Uvicorn
 uvicorn main:app --host 0.0.0.0 --port $PORT --reload
